02efeeae70cc14b177ea9b43f9be308a945f3ace
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2021-01-03 00:39:01 -0300

    Bugfix de node-sass y agregados algunos estilos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MusicPlayerApp } from './MusicPlayerApp';
 
+import './styles/styles.scss'
+
 ReactDOM.render(
     <MusicPlayerApp />,
   document.getElementById('root')
